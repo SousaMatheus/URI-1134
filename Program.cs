9f41f9f42dbfa765c2b68c73a686f769a6a48580
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-12-16 10:38:34 -0300

    criado solução

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace URI_1134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alcool, gasolina, diesel;
+            alcool = 0;
+            gasolina = 0;
+            diesel = 0;
+            int valor = int.Parse(Console.ReadLine());
+
+            while (valor != 4)
+            {
+                
+                if (valor == 1)
+                {
+                    alcool++;
+                }
+                if (valor == 2)
+                {
+                    gasolina++;
+                }
+                if (valor == 3)
+                {
+                    diesel++;
+                }
+                valor = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
